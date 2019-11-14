792d1dcc0ce2ddbfa79018b53a2bb6d9775fd8a4
DmytroSavchuk <SavchukD7@gmail.com>
2019-11-15 00:02:46 +0200

    Initial commit

---
+++ src/main/java/com/bestfriends/comandproject/ComandprojectApplication.java
@@ -0,0 +1,13 @@
+package com.bestfriends.comandproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComandprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComandprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bestfriends/comandproject/ComandprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestfriends.comandproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComandprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
