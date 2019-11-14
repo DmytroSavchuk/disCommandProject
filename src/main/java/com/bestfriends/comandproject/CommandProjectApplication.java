d7d04575d5d921960f62bcf3cceb38fc6836670c
DmytroSavchuk <SavchukD7@gmail.com>
2019-11-15 00:06:05 +0200

    Rename all bad classes

--- src/main/java/com/bestfriends/comandproject/CommandProjectApplication.java
+++ src/main/java/com/bestfriends/comandproject/CommandProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ComandprojectApplication {
+public class CommandProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ComandprojectApplication.class, args);
+        SpringApplication.run(CommandProjectApplication.class, args);
     }
 
 }
